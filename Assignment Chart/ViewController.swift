c1653fcdd476e94c6bd8128d02d173d115f57a75
sandeeprana011 <sandeep.rana011@gmail.com>
2017-11-10 22:40:59 +0530

    Initial commit with initialization

--- Assignment Chart/ViewController.swift
+++ Assignment Chart/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
